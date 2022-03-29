186739751c736a85a3e3f2979284e670269c4403
hechongwei <1186105627@qq.com>
2022-03-29 23:41:00 +0800

    2020.3.29

--- src/main/java/com/he/community/MySpringbootApplication.java
+++ src/main/java/com/he/community/MySpringbootApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringbootApplication {
 
-    //1
-    //1
     public static void main(String[] args) {
         SpringApplication.run(MySpringbootApplication.class, args);
     }
